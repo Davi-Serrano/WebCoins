f4b2a7e1bda74b11b83f87d8e4a281653caf64b3
Davi Serrano <daviscardoso2901@gmail.com>
2021-08-02 11:54:26 -0300

    Coins Context e dados da Api resgatados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import CoinsProvider from "./Context/Coins";
 
+import Coins from "./Pages/Coins/index.js"; 
 
 function App() {
   return (
+    <CoinsProvider>
+
     <div className="App">
       <h1>Hellow</h1>
+
+      <Coins />                    
     </div>
+    </CoinsProvider>
   );
 }
 
---
+++ src/Context/Coins.js
@@ -0,0 +1,31 @@
+import { useState, createContext, useContext} from "react"
+
+export const CoinsContext = createContext();
+
+export default function CoinsProvider({ children }){
+
+
+    const [ coins, setCoins ] = useState();
+
+    return(
+
+        <CoinsContext.Provider
+        value={{
+            coins,
+            setCoins
+        }}
+        > 
+            
+            {children}    
+        
+         </CoinsContext.Provider>
+
+    )
+}
+
+export function useCoins(){
+    const context = useContext(CoinsContext)
+    const { coins, setCoins }  = context ;
+
+    return  { coins, setCoins };
+}
