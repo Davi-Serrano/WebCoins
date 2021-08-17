561f44dd778d1e8d01f15047e93656319eaa7c58
Davi Serrano <daviscardoso2901@gmail.com>
2021-08-17 16:33:47 -0300

    Header condicional add

--- src/Componets/Header/index.js
+++ src/Componets/Header/index.js
@@ -1,11 +1,31 @@
 import { TopHeader } from "./styled"
 import  BtnTheme  from "../ButtonTheme/index";
+import { AiOutlineLogin } from "react-icons/ai";
+import { AiOutlineLogout } from "react-icons/ai";
+
 
 export default function Header() {
-    return (
-        <TopHeader>
-            <h1>Web Coins</h1>
-            < BtnTheme/>
-        </TopHeader>
-    )
-}
+
+    const login = localStorage.getItem("@Webcoins: log")
+
+    function handleLogout(){
+        localStorage.setItem("@Webcoins: log", "no")
+    }
+
+    if(login === "yes"){
+        return(
+            <TopHeader>
+                <h1>Web Coins</h1> <a href="/login"> <AiOutlineLogout onClick={handleLogout} alt="Logout" size={35}/> Logout</a>
+                < BtnTheme alt="BtnTheme"/>
+            </TopHeader>
+            )
+
+
+}     
+        return (
+            <TopHeader>
+                <h1>Web Coins</h1> <a href="/login"> <AiOutlineLogin alt="Login" size={35} />  Login</a>
+                < BtnTheme alt="BtnTheme"/>
+            </TopHeader>
+
+        )}
--- src/Componets/Header/styled.js
+++ src/Componets/Header/styled.js
@@ -2,9 +2,20 @@ import styled from "styled-components";
 
 export const TopHeader = styled.div`
         display: flex;
-        justify-content: center;
+        justify-content: space-around;
+        align-items: center;
         padding: 1em;
         background:  ${props => props.theme.colors.background} ;
         color: ${props => props.theme.colors.color};
-
+       
+        a{
+                display: flex;
+                align-items: center;
+                color: ${props => props.theme.colors.color};
+                
+                &:hover{
+                        transform: translateY(-3px);
+                        opacity: 0.5;
+                }
+        }
 `
