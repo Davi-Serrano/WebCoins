5977d85a76748ebbbdc607e706fc3304febfd525
Davi Serrano <daviscardoso2901@gmail.com>
2021-08-17 14:04:14 -0300

    lógica register criada

--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -1,7 +1,29 @@
-import { SingIn, Box, Input ,Btn } from "./styled"
+import { useState } from "react"
+import { SingIn, Box, Input ,Btn, Error } from "./styled"
 
 export default function Register() {
-    
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [password2, setPassword2] = useState("")
+    const [error, setError] = useState("")
+
+    function handleRegister(){
+
+        if(!email || !password || !password2){
+            setError("Por favor preencha todos os campos")
+        }
+        if(password !== password2){
+            setError("As senhas são diferentes")
+        }
+        if(password.length < 5){
+            setError("Senha muito curta")
+        }
+        localStorage.setItem("@Webcoin: Email", email)
+        localStorage.setItem("@Webcoin: Password", password)
+    }
+
+
         return (
         
         <SingIn>
@@ -12,17 +34,19 @@ export default function Register() {
                     
                         <label>Email</label>
                         <Input>
-                        <input type="email" />
+                        <input type="email" onChange={ (e) => setEmail(e.target.value)}/>
                         </Input>
                         <label>Password</label>
                         <Input>
-                        <input type="password"/>
+                        <input type="password" onChange={ (e) => setPassword(e.target.value)}/>
                         </Input>
                         <label>Password</label>
                         <Input>
-                        <input type="password"/>
+                        <input type="password" onChange={ (e) => setPassword2(e.target.value)}/>
                         </Input>
-                        <Btn>Register</Btn>  
+                        <Error>  {error} </Error>
+                        <Btn onClick={handleRegister}>Register</Btn>
+                       
 
             </Box>
       
